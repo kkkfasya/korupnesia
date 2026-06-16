81c5e8413aed7a0063badbf4f739bc94b1b1f3b0
Kamal Fasya <kamalfasya01@gmail.com>
2026-06-16 09:32:21 +0700

    chunking data done, only need to process multithreaded now

--- scripts/dispatcher.py
+++ scripts/dispatcher.py
@@ -5,23 +5,22 @@ parsed data will be keep in memory and will be written to database
 it also logs the infos
 """
 
-from cmath import log
-
 from pathlib import Path
 from loguru import logger
 import sys
+import os
 import json
 import traceback
 
 # absolute path to the project root
 PROJECT_ROOT = Path(__file__).resolve().parent.parent
+DATADIR = Path(f"{PROJECT_ROOT}/data")
 LOGFORMAT = "<level>[{level}]</level> | <green>{time}</green> | {message}"
 LOGSTASH_LOGFILE = Path(f"{PROJECT_ROOT}/logs/dispatcher_serializable.log")
 
 
 def logstash_sink(message):
     record = message.record
-
     event = {
         "@timestamp": record["time"].isoformat(),
         "log.level": record["level"].name.lower(),
@@ -67,4 +66,20 @@ logger.add(
 # for logstash
 logger.add(logstash_sink)
 
-logger.info("HALLo")
+if sys._is_gil_enabled():  # ty: ignore
+    logger.warning("GIL is enabled, might not get the best performance")
+
+
+def get_data_chunk(dir) -> list[list[Path]]:
+    d = Path(dir)
+    if not d.is_dir():
+        return [[Path("").chmod]]
+
+    # the fuck do you mean by int | None lol
+    chunk_size = os.cpu_count() // 2  # ty: ignore
+    items = list(d.iterdir())
+
+    return [items[i : i + chunk_size] for i in range(0, len(items), chunk_size)]
+
+
+print(get_data_chunk(DATADIR))
