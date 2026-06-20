59466146d5e59633e118082bd964d36e01e28f24
Kamal Fasya <kamalfasya01@gmail.com>
2026-06-21 01:56:29 +0700

    feat: multithreaded dispatcher, and reconfigured logger

---
+++ src/main.py
@@ -0,0 +1,12 @@
+import dispatcher
+from pathlib import Path
+
+def main():
+    datapath = Path(f"{Path(__file__).parent.parent}/data")
+    print(datapath)
+    datachunk = dispatcher.get_data_chunk(datapath)
+    if not datachunk:
+        return
+    dispatcher.batch_process(datachunk)
+
+main()
